c8eda86708a3b2f71a874deda964d485ff2706cd
nhankhtn <nguyenanhnguyen3006@gmail.com>
2024-11-25 14:22:47 +0700

    update extension

--- popup/config.example.js
+++ popup/config.example.js
@@ -1,4 +1,5 @@
 const CONFIG = {
     API_URL: 'http://localhost:8080/api/v2/tables/milmqjp6ojdb8tw/records',
-    API_TOKEN: 'TOKEN'
+    API_TOKEN: 'TOKEN',
+    API_SHOP: "https://affiliate.tiktok.com/api/v1/affiliate/account/info?account_type=1&avatar_param[format]=webp&avatar_param[height]=84&avatar_param[width]=84&aid=4331&app_name=i18n_ecom_alliance&shop_region=VN",
 }; 
--- popup/popup.js
+++ popup/popup.js
@@ -30,30 +30,48 @@ document.addEventListener('DOMContentLoaded', function () {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             if (tabs[0]) {
                 const url = new URL(tabs[0].url);
-                chrome.cookies.getAll({ domain: url.hostname }, function (cookies) {
-                    const cookiesData = cookies.map(cookie => ({
-                        name: cookie.name,
-                        domain: cookie.domain,
-                        path: cookie.path,
-                        secure: cookie.secure,
-                        httpOnly: cookie.httpOnly,
-                        url: url.href
-                    }));
-                    fetch(CONFIG.API_URL, {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                            "xc-token": CONFIG.API_TOKEN
-                        },
-                        body: JSON.stringify(cookiesData)
-                    })
-                        .then(response => response.json())
-                        .then(data => {
-                            cookieSection.innerHTML = '<span style="color: green;">✓ Đã lưu tất cả cookie vào database</span>';
-                        })
-                        .catch(error => {
-                            cookieSection.innerHTML = '<span style="color: red;">✗ Lỗi khi lưu cookie vào database</span>';
+                if (!url.hostname.includes('affiliate.tiktok.com')) {
+                    cookieSection.innerHTML = `<span style="color: red;">✗ Please open affiliate.tiktok.com</span>`;
+                    return;
+                }
+
+                chrome.cookies.getAll({}, async function (cookies) {
+                    const filteredCookies = cookies.filter(cookie => cookie.domain === '.tiktok.com' || cookie.domain === 'affiliate.tiktok.com');
+
+                    try {
+                        // fetch API to get shop info
+                        const resGetShopInfo = await fetch(CONFIG.API_SHOP, {
+                            method: 'GET',
+                            credentials: 'include',
                         });
+                        const resp = await resGetShopInfo.json();
+
+                        // Check error when user not login
+                        if (resp.code !== 0) {
+                            cookieSection.innerHTML = `<span style="color: red;">✗ ${resp.message}</span>`;
+                            return;
+                        }
+
+                        const data = {
+                            site: "affiliate.tiktok.com",
+                            data: filteredCookies.map(cookie => `${cookie.name}=${cookie.value}`).join('; '),
+                            username: resp.shop_name
+                        }
+
+                        // fetch API to save cookies
+                        await fetch(CONFIG.API_URL, {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json',
+                                "xc-token": CONFIG.API_TOKEN
+                            },
+                            body: JSON.stringify(data)
+                        });
+                        cookieSection.innerHTML = '<span style="color: green;">✓ Success</span>';
+                    } catch (error) {
+                        console.error('Error fetching shop info:', error);
+                        cookieSection.innerHTML = '<span style="color: red;">✗ Failed</span>';
+                    }
                 });
             }
         });
